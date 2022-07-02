30556652f67e23ea18aa7aa4d69ac4e934e8dbad
KateAmador <kateamador13@gmail.com>
2022-07-02 09:26:10 -0500

    actualiazacion

--- src/cicloWhile.java
+++ src/cicloWhile.java
@@ -1,8 +1,10 @@
 public class cicloWhile {
 
-    public cicloWhile(){
-        primerEjemplo();
-        segundoEjemplo();
+    public cicloWhile() {
+        //primerEjemplo();
+        //segundoEjemplo();
+        //tercerEjemplo();
+        cuartoEjemplo();
     }
 
     public void primerEjemplo() {
@@ -32,9 +34,50 @@ public class cicloWhile {
             System.out.println("Numero: " + numero);
             numero--;
 
+        }
+    }
+
+    public void tercerEjemplo() {
+
+        /*Imprimir:
+        1 - D
+        2 - L
+        3 - D
+        4 - L
+        Hasta el 100
+         */
+
+        int numero = 1;
+        String d = "D", l = "L";
+
+        while (numero <= 100) {
+            if (numero % 2 == 0) {
+                System.out.println(numero + " - " + l);
+            } else {
+                System.out.println(numero + " - " + d);
             }
+            numero++;
         }
 
+    }
+
+
+    public void cuartoEjemplo(){
+
+        for(int i = 1; i <= 5; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for(int i = 1; i <= 5; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
 
     public static void main(String[] args) {
        new cicloWhile();
