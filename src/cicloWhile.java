eb2ae61da26e470f4843903bce79ecdf90f2bdc2
KateAmador <kateamador13@gmail.com>
2022-07-02 08:36:17 -0500

    actualiazacion

--- src/cicloWhile.java
+++ src/cicloWhile.java
@@ -1,19 +1,42 @@
 public class cicloWhile {
-    public static void main(String[] args) {
 
+    public cicloWhile(){
+        primerEjemplo();
+        segundoEjemplo();
+    }
+
+    public void primerEjemplo() {
         //Ciclo while
         //pintar los numeros del 1 al 10
 
         boolean noHaTerminado = true;
         int numero = 0;
 
-        while (noHaTerminado){
+        while (noHaTerminado) {
             System.out.println("Numero: " + numero);
             numero++;
 
-            if (numero >= 10){
+            if (numero >= 10) {
                 noHaTerminado = false;
             }
         }
+    }
+
+    public void segundoEjemplo() {
+        //Ciclo while
+        //pintar los numeros del 10 al 1
+
+        int numero = 10;
+
+        while (numero >= 1) {
+            System.out.println("Numero: " + numero);
+            numero--;
+
+            }
+        }
+
+
+    public static void main(String[] args) {
+       new cicloWhile();
     }
 }
